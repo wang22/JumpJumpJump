89c71216bebb2afd693ac895e88b92522bb5287a
小王王 <pause22@163.com>
2020-08-26 12:27:28 +0800

    settings

--- assets/script/player.ts
+++ assets/script/player.ts
@@ -4,10 +4,20 @@ const { ccclass, property } = cc._decorator;
 export default class NewClass extends cc.Component {
 
     @property(cc.Prefab)
-    dustPrefab: cc.Prefab
+    dustPrefab: cc.Prefab;
 
     @property(cc.Prefab)
-    jumpDustPrefab: cc.Prefab
+    jumpDustPrefab: cc.Prefab;
+
+    /**
+     * 最高可跳跃次数
+     */
+    totalJumpCount: number = 2;
+
+    /**
+     * 跳跃次数
+     */
+    jumpCount: number = 0;
 
     onLoad() {
         this.node.runAction(cc.repeatForever(cc.moveBy(0.5, 100, 0)));
@@ -19,10 +29,15 @@ export default class NewClass extends cc.Component {
     }
 
     onJump() {
+        if (this.totalJumpCount == this.jumpCount) {
+            return ;
+        }
+        this.jumpCount ++;
         const anim = this.node.getComponent(cc.Animation);
         anim.play("player1_jump");
         const seq = cc.sequence(cc.jumpBy(0.5, cc.v2(0, 0), 60, 1), cc.callFunc(() => {
             anim.play("player1_run");
+            this.jumpCount --;
         }))
         this.node.runAction(seq);
     }
