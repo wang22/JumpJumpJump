1637ef488a8558d80ce9012dd329157c95736223
小王王 <pause22@163.com>
2020-08-29 15:40:49 +0800

    ground prefab完成

--- assets/script/factory/ground-factory.ts
+++ assets/script/factory/ground-factory.ts
@@ -2,15 +2,13 @@ import Factory from './factory';
 
 class GroundFactory implements Factory {
     build(data: any, parentNode: cc.Node): void {
-        // cc.resources.load("Prefab/ground", (err: Error, prefab: cc.Prefab) => {
-        //     const node = cc.instantiate(prefab);
-        //     node.width = data.width;
-        //     node.height = data.height;
-        //     node.setPosition(data.x, data.y);
-        //     node.parent = parentNode;
-        // })
-        console.log(cc.resources);
-        
+        cc.resources.load("prefab/ground", (err: Error, prefab: cc.Prefab) => {
+            const node = cc.instantiate(prefab);
+            node.width = data.width;
+            node.height = data.height;
+            node.setPosition(data.x, data.y);
+            node.parent = parentNode;
+        })
     }
 }
 
